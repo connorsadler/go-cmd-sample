f7375d2597cacc4f729e5ba3b88fedf4c396f5c4
Connor <connor.sadler@gmail.com>
2024-10-02 16:31:08 +0100

    removed extraneous files from step_1 branch copied README.md changes from main corrected quoting

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	// usefulcode "go-cmd-sample"
 )
 
 func main() {
 	fmt.Println("go-cmd-sample - main running")
-
-	// usefulResult := usefulcode.MyUsefulFunction()
-	// fmt.Printf("usefulResult: %s\n", usefulResult)
 
 	fmt.Println("go-cmd-sample - main done")
 }
